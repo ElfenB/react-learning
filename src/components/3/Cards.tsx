bdd7581ef4405b12c6149038fbd941ea1c1d6e53
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2022-10-12 11:26:08 +0200

    changed content

--- src/components/3/Cards.tsx
+++ src/components/3/Cards.tsx
@@ -4,7 +4,7 @@ export default function Cards(props: { data: card[] }) {
   return (
     <section className="cards">
       {props.data.map((c) => (
-        <Card card={c} />
+        <Card card={c} key={c.img} />
       ))}
     </section>
   )
--- src/components/3/Three.tsx
+++ src/components/3/Three.tsx
@@ -3,8 +3,10 @@ import './3.scss'
 import Cards from './Cards'
 import Hero from './Hero'
 import Navbar from './Navbar'
+import bike from './assets/bike.svg'
 import { card } from './Card'
 import katy from './assets/katy.svg'
+import wedding from './assets/wedding.svg'
 
 export default function Three() {
   return (
@@ -23,25 +25,25 @@ const testdata: card[] = [
     rating: 5,
     reviews: 6,
     country: 'USA',
-    description: 'Life lessons with Katy are great.',
+    description: 'Life lessons with Katy are great',
     price: 135.99,
   },
   {
-    openSpots: 0,
-    img: katy,
-    rating: 5,
-    reviews: 6,
-    country: 'USA',
-    description: 'Life lessons with Katy are great.',
-    price: 135.99,
+    openSpots: 20,
+    img: wedding,
+    rating: 4.5,
+    reviews: 30,
+    country: 'Germany',
+    description: 'Learn wedding photography',
+    price: 124.99,
   },
   {
     openSpots: 0,
-    img: katy,
-    rating: 5,
-    reviews: 6,
-    country: 'USA',
-    description: 'Life lessons with Katy are great.',
-    price: 135.99,
+    img: bike,
+    rating: 4.8,
+    reviews: 2,
+    country: 'England',
+    description: 'Group Mountain Biking',
+    price: 49.99,
   },
 ]
