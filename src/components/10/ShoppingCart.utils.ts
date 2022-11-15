af206141eb00a017e13e41ee1a56ecfda2e462de
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2022-11-15 09:38:29 +0100

    reverted test changes

--- src/components/10/ShoppingCart.utils.ts
+++ src/components/10/ShoppingCart.utils.ts
@@ -8,7 +8,7 @@ export const useDispatchAction = (action: (params: any) => AnyAction, params: an
   return useCallback(() => dispatch(action(params)), [action, dispatch, params]);
 };
 
-export function useDispatchAction2<test = any>(action: (args: test) => AnyAction, args: test): () => void {
+export function useDispatchAction2<T = any>(action: (args: T) => AnyAction, args: T): () => void {
   const dispatchAction = useDispatchActionCallback(action);
 
   return useCallback(() => {
