f108bd3b2d8eaf9ceab9eae02deb4246c90274f9
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2022-12-15 17:18:50 +0100

    fixed default colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,6 +1,16 @@
 import { createTheme } from '@mui/material/styles';
 
 export const theme = createTheme({
+  palette: {
+    background: {
+      default: 'var(--background-color)',
+      paper: 'var(--background-color)',
+    },
+    text: {
+      primary: 'var(--color)',
+      secondary: 'var(--text-shade)',
+    },
+  },
   typography: {
     fontFamily: ['Inter'].join(','),
   },
