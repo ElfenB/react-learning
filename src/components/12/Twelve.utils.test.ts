9b89e715018dc4c65c8090d9bffefff47a0fee47
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-01-05 17:53:07 +0100

    configured vitest to work

--- src/components/12/Twelve.utils.test.ts
+++ src/components/12/Twelve.utils.test.ts
@@ -1,14 +1,14 @@
+import { describe, expect, test } from 'vitest';
 import { formatNatoDate } from './Twelve.utils';
 
-// TODO: Jest not working
-describe('test', () => {
+describe('formatNatoDate', () => {
   // Expect 930 to get 9:30
-  test('formatDate', () => {
+  test('transform values with 3 digits', () => {
     const res = formatNatoDate(930);
-    expect(res).toBe('9:30');
+    expect(res).toBe('09:30');
   });
   // Expect 1030 to get 10:30
-  test('formatDate', () => {
+  test('transform values with 4 digits', () => {
     const res = formatNatoDate(1030);
     expect(res).toBe('10:30');
   });
