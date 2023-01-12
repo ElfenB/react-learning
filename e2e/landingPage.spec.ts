a3461f20f266e8662b0fc2e8ecb05401678f9e47
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-01-12 17:59:03 +0100

    wrote first tests

--- e2e/landingPage.spec.ts
+++ e2e/landingPage.spec.ts
@@ -5,4 +5,23 @@ test('should display correct title', async ({ page }) => {
   await page.goto('/');
 
   await expect(page).toHaveTitle('Ben learns React');
+  await expect.soft(page.getByRole('heading', { name: 'Navigation' })).toBeVisible();
+});
+
+test.describe('should display the heading', () => {
+  test.use({
+    viewport: {
+      height: 1080,
+      width: 1920,
+    },
+  });
+
+  test('should display the right heading', async ({ page }) => {
+    await page.goto('/');
+    await page
+      .getByRole('link', { name: 'Image of Learning path Learning path See navigation for complete learning path.' })
+      .click();
+    await page.getByRole('link', { name: '/one' }).click();
+    await expect.soft(page.getByRole('heading', { name: 'React Fun Facts' })).toBeVisible();
+  });
 });
