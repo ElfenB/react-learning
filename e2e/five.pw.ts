c4c33d7016a1359cd2fde81b661475bce75c46e5
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2023-01-16 15:08:08 +0100

    refactored tests

--- e2e/five.pw.ts
+++ e2e/five.pw.ts
@@ -1,11 +1,14 @@
 import { expect, test } from '@playwright/test';
 
 test('Five form should work correctly', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
+  // Navigate to five from the main navigation
   await page
     .getByRole('link', { name: 'Image of Learning path Learning path See navigation for complete learning path.' })
     .click();
   await page.getByRole('link', { name: '/five' }).click();
+
+  // Fill out the form
   await page.getByPlaceholder('First Name').click();
   await page.getByPlaceholder('First Name').fill('My Playwright');
   await page.getByPlaceholder('First Name').press('Tab');
@@ -14,10 +17,15 @@ test('Five form should work correctly', async ({ page }) => {
   await page.getByPlaceholder('Email').fill('my.playwright@bot.com');
   await page.getByPlaceholder('Email').press('Tab');
   await page.getByPlaceholder('Comments').fill('no comment to this');
+  // Check all the boxes
   await page.locator('form div').filter({ hasText: 'Are you friendly?' }).click();
   await page.getByLabel('Part-time').check();
   await page.locator('#favColor').selectOption('indigo');
+
+  // Send it
   await page.getByRole('button', { name: 'Send' }).click();
+
+  // Check result
   expect(await page.getByText('{"comments":"').allInnerTexts()).toContain(
     '{"comments":"no comment to this","email":"my.playwright@bot.com","employment":"part-time","favColor":"indigo","firstName":"My Playwright","isFriendly":true,"lastName":"Bot"}'
   );
--- e2e/six.pw.ts
+++ e2e/six.pw.ts
@@ -1,19 +1,28 @@
 import { expect, test } from '@playwright/test';
 
-test('Five form should work correctly', async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+test('Six form should work correctly', async ({ page }) => {
+  await page.goto('/');
+  // Navigate to six from the main navigation
   await page
     .getByRole('link', { name: 'Image of Learning path Learning path See navigation for complete learning path.' })
     .click();
   await page.getByRole('link', { name: '/six' }).click();
+
+  // Fill out the form
   await page.getByPlaceholder('Email').fill('playwright@bot.com');
   await page.getByPlaceholder('Email').press('Tab');
   await page.locator('input[name="password"]').fill('test123');
   await page.locator('input[name="password"]').press('Tab');
   await page.getByPlaceholder('Repeat password').fill('test123');
+
+  // Check all the boxes
   await page.getByLabel('I want to join the newsletter').uncheck();
+
+  // Send it
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  // Check result
   expect(
     await page.getByText('{"email":"').allInnerTexts()
   ).toContain('{"email":"playwright@bot.com","newsletter":false,"password":"test123","passwordRepeat":"test123"}');
-  await page.getByRole('button', { name: 'Login' }).click();
 });
