ecc761d102661aad1edce54eb04c8c506d9fe978
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2024-02-05 17:22:15 +0100

    feat: added semantic release

---
+++ .releaserc.cjs
@@ -0,0 +1,12 @@
+/** @type {import('semantic-release').Options}  */
+
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}' }],
+  ],
+  branches: ['main'],
+};
