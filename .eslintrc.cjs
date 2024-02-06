e9e8c119deb9226b3d05a62f5a696abba6d6adcb
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2024-02-06 16:01:26 +0100

    style: added eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
 /** @type {import('eslint').ESLint.ConfigData}  */
 module.exports = {
   extends: [
-    '@boehringer-ingelheim/eslint-config/base',
+    '@boehringer-ingelheim/eslint-config/base/strict',
     '@boehringer-ingelheim/eslint-config/react',
     // NOTE: Prettier has to be the last one to work
     'prettier',
   ],
+  plugins: ['react-refresh'],
   rules: {
     'sonarjs/cognitive-complexity': 'warn',
     'sonarjs/no-collapsible-if': 'warn',
@@ -16,8 +17,11 @@ module.exports = {
     'sonarjs/prefer-single-boolean-return': 'off',
     'sonarjs/prefer-immediate-return': 'off',
     // No error for wrong sorting
-    'react/jsx-sort-props': 'warn',
-    'sort-keys-plus/sort-keys': 'warn',
-    'import/order': 'warn',
+    'perfectionist/sort-named-imports': 'warn',
+    'perfectionist/sort-object-types': 'warn',
+    'perfectionist/sort-objects': 'warn',
+    'perfectionist/sort-jsx-props': 'warn',
+    // https://github.com/vitejs/vite-plugin-react-swc#consistent-components-exports
+    'react-refresh/only-export-components': 'warn',
   },
 };
