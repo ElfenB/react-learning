558c8def108fe9c956de215d5a5fd762f26912a4
Benjamin Elfen <benjamin.elfen@boehringer-ingelheim.com>
2024-02-07 10:33:03 +0100

    ci: added title for changelog

--- .releaserc.cjs
+++ .releaserc.cjs
@@ -4,7 +4,7 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    ['@semantic-release/changelog', { changelogTitle: '# Changelog' }],
     '@semantic-release/npm',
     ['@semantic-release/git', { message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}' }],
   ],
